17903fa3564bb7828c9ba83702024345ac17cbfd
Gregory Timmons <gbtimmon@ncsu.edu>
2016-10-09 11:09:13 -0400

    autosave

--- Code/4/Table2.py
+++
@@ -1,7 +0,0 @@
-
-class Table( list ) : 
-  def __init__ ( i , data ) :
-    super(data) 
-    i.head = []
-
-class Header( list ) : 
--- Code/4/test.py
+++
@@ -1,3 +0,0 @@
-class Test( list ) : 
-  def __init__( i, data ) : 
-    map( lambda x : i.append(x), data ) 
