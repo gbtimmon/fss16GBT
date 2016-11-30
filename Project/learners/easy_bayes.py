bda1ebc32617094f7f701d9b2c333ab6a0f84e43
Gregory Timmons <gbtimmon@ncsu.edu>
2016-11-30 12:15:57 -0500

    zzz

--- Project/learners/easy_bayes.py
+++ Project/learners/easy_bayes.py
@@ -5,17 +5,15 @@ import sklearn as sk
 from scipy.io import arff
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import GaussianNB
+from Table import Reader, Table 
 
 
 
 if __name__ == '__main__' : 
-  data, meta = arff.loadarff( open( 'nasa93.arff' ) ) 
-  data = np.array([ list(x) for x in data ], dtype=(np.str_) )
-  print( meta ) 
-  iv =  data[:, [ 5,6,7,8,11,12,13,14,15,17,18,19,20,22]] 
-  dv =  data[:, 23 ]
 
-  gnb = GaussianNB()
-  y_pred = gnb.fit( iv, dv ).predict( iv ) 
 
-  print( y_pred )
+  t = Reader( "../data/nasa93.arff", sep=",").table()
+
+  print( *t[1:3] ) 
+
+  
